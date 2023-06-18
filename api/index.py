10889775814ca2f8a8fd5873e9c703e8a44b6be1
ashis-solomon <ashissolomon24@gmail.com>
2023-06-18 23:55:32 +0530

    update

--- api/index.py
+++ api/index.py
@@ -1,22 +1,56 @@
-from http.server import BaseHTTPRequestHandler
-import json
+from flask import Flask, jsonify, request
+import itertools
+import random
 import datetime
 
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        response = self._generate_response()
-        self._send_response(200, response)
+app = Flask(__name__)
 
-    def _generate_response(self):
-        current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        response = {
-            'message': 'Hello, world!',
-            'current_time': current_time
-        }
-        return json.dumps(response)
+def generate_distance_matrix(num_cities):
+    distances = [[0] * num_cities for _ in range(num_cities)]
+    for i in range(num_cities):
+        for j in range(i+1, num_cities):
+            distance = random.randint(1, 100)
+            distances[i][j] = distance
+            distances[j][i] = distance
+    return distances
 
-    def _send_response(self, status_code, response):
-        self.send_response(status_code)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(response.encode('utf-8'))
+def tsp_brute_force(distances):
+    num_cities = len(distances)
+    cities = range(num_cities)
+    shortest_path = None
+    shortest_distance = float('inf')
+
+    for path in itertools.permutations(cities):
+        current_distance = 0
+        for i in range(num_cities - 1):
+            current_distance += distances[path[i]][path[i+1]]
+        current_distance += distances[path[-1]][path[0]]
+
+        if current_distance < shortest_distance:
+            shortest_distance = current_distance
+            shortest_path = path
+
+    return shortest_path, shortest_distance
+
+@app.route('/api')
+def api():
+    current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    response = {
+        'message': 'Hello, world!',
+        'current_time': current_time
+    }
+    return jsonify(response)
+
+@app.route('/api/tsp/<int:num_cities>', methods=['GET'])
+def tsp(num_cities):
+    distances = generate_distance_matrix(num_cities)
+
+    shortest_path, shortest_distance = tsp_brute_force(distances)
+
+    response = {
+        'distances': distances,
+        'shortest_path': shortest_path,
+        'shortest_distance': shortest_distance
+    }
+
+    return jsonify(response)
