90b188f0ce4eb6bc932d9befb590254c6dd06d86
ashis-solomon <ashissolomon24@gmail.com>
2023-06-18 15:32:25 +0530

    update

--- api/index.py
+++ api/index.py
@@ -1,10 +1,22 @@
 from http.server import BaseHTTPRequestHandler
+import json
+import datetime
 
-class handler(BaseHTTPRequestHandler):
-
+class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        response = self._generate_response()
+        self._send_response(200, response)
+
+    def _generate_response(self):
+        current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        response = {
+            'message': 'Hello, world!',
+            'current_time': current_time
+        }
+        return json.dumps(response)
+
+    def _send_response(self, status_code, response):
+        self.send_response(status_code)
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+        self.wfile.write(response.encode('utf-8'))
